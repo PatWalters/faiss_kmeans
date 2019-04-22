084ac501d79e9666e7fb7ec3bbd2f2fb3b8e56a3
Patrick Walters <wpwalters@gmail.com>
2019-04-22 08:25:55 -0400

    Update gen_fp.py

--- gen_fp.py
+++ gen_fp.py
@@ -7,7 +7,25 @@ import numpy as np
 from rdkit import Chem, DataStructs
 from rdkit.Chem import rdMolDescriptors as rdmd
 from tqdm import tqdm
+from functools import wraps
+from time import time
 
+
+def timing(f):
+    """
+    Decorator to measure execution time, adapted from
+    # https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
+    # https://codereview.stackexchange.com/questions/169870/decorator-to-measure-execution-time-of-a-function
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time()
+        result = f(*args, **kwargs)
+        end = time()
+        print(f.__name__, f"Elapsed time: {end - start:.2f} sec")
+        return result
+
+    return wrapper
 
 @timing
 def make_np_array(lst, dtype=np.float32):
