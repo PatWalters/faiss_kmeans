be9ae2a002598020463ad54e6656b269c081e09f
Patrick Walters <wpwalters@gmail.com>
2019-04-22 08:23:40 -0400

    Update gen_fp.py

--- gen_fp.py
+++ gen_fp.py
@@ -1,70 +1,84 @@
 #!/usr/bin/env python
 
 import sys
-import numpy as np
-from rdkit.Chem import rdMolDescriptors as rdmd
-from rdkit import Chem, DataStructs
-import numpy as np
-import time
-from tqdm import tqdm
+import faiss
 import h5py
+import csv
 from functools import wraps
 from time import time
 
-# https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
-# https://codereview.stackexchange.com/questions/169870/decorator-to-measure-execution-time-of-a-function
+
 def timing(f):
+    """
+    Decorator to measure execution time, adapted from
+    # https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
+    # https://codereview.stackexchange.com/questions/169870/decorator-to-measure-execution-time-of-a-function
+    """
     @wraps(f)
     def wrapper(*args, **kwargs):
         start = time()
         result = f(*args, **kwargs)
         end = time()
-        print(f.__name__,f"Elapsed time: {end-start:.2f} sec")
+        print(f.__name__, f"Elapsed time: {end - start:.2f} sec")
         return result
+
     return wrapper
 
-@timing
-def make_np_array(lst):
-    return np.array(lst,dtype=np.float32)
 
 @timing
-def save_data(fp_array,smiles_list,name_list,outfile_name):
-    h5f = h5py.File(outfile_name, 'w')
-    dt = h5py.special_dtype(vlen=bytes)
-    h5f.create_dataset('fp_list', data=fp_array)
-    h5f.create_dataset('smiles_list', (len(smiles_list),1),dt, smiles_list)
-    h5f.create_dataset('name_list', (len(name_list),1),dt, name_list)
+def get_centers(num_centroids, dist, cluster_id):
+    """
+    Find the most central molecule in each cluster
+    :param num_centroids: number of cluster centers
+    :param dist: list of distances from the cluster center
+    :param cluster_id: list of cluster identifiers
+    :return:
+    """
+    nearest = [[sys.float_info.max, -1]] * num_centroids
+    for i, (d, c) in enumerate(zip(dist, cluster_id)):
+        if d < nearest[c][0]:
+            nearest[c] = [d, i]
+    return nearest
+
+
+@timing
+def faiss_kmeans(infile, ncentroids, niter=20):
+    """
+    K-Means clustering with FAISS
+    :param infile: Input file name
+    :param ncentroids: desired number of clusters
+    :param niter: maximum number of iterations
+    :return: None
+    """
+    h5f = h5py.File(infile, 'r')
+    x = h5f['fp_list'][:]
+    smiles_list = h5f['smiles_list'][:]
+    name_list = h5f['name_list'][:]
     h5f.close()
 
-@timing
-def generate_fingerprints(infile_name):
-    ifs = open(infile_name)
-    fp_list = []
-    smiles_list = []
-    name_list = []
-    for line in tqdm(ifs):
-        toks = line.strip().split(" ",1)
-        if len(toks) >= 2:
-            smiles, name = toks
-            mol = Chem.MolFromSmiles(smiles)
-            if mol:
-                fp = rdmd.GetMorganFingerprintAsBitVect(mol, 2,256)
-                arr = np.zeros((1,))
-                DataStructs.ConvertToNumpyArray(fp, arr)
-                fp_list.append(arr)
-                smiles_list.append(smiles.encode("ascii", "ignore"))
-                name_list.append(name.encode("ascii", "ignore"))
-    return fp_list,smiles_list,name_list
+    verbose = True
+    d = x.shape[1]
+    kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose)
+    kmeans.train(x)
+    D, I = kmeans.index.search(x, 1)
+    writer = csv.writer(open("detail.csv", "w"))
+    writer.writerow(["SMILES", "NAME", "DIST", "CLUSTER"])
+    for smiles, name, d, i in zip(smiles_list, name_list, [x[0] for x in D], [x[0] for x in I]):
+        writer.writerow([smiles[0].decode('utf-8'), name[0].decode('utf-8'), d, i])
+    dist_list = get_centers(ncentroids, [x[0] for x in D], [x[0] for x in I])
+    ofs = open("centers.smi", "w")
+    for cluster_idx, (_, c) in enumerate(dist_list, 1):
+        print(smiles_list[c][0].decode('utf-8'), name_list[c][0].decode('utf-8'), cluster_idx, file=ofs)
+    ofs.close()
+
 
 @timing
 def main():
-    fp_list, smiles_list, name_list = generate_fingerprints(sys.argv[1])
-    outfile_name = sys.argv[2]
-    fp_array = make_np_array(fp_list)
-    save_data(fp_array,smiles_list,name_list,outfile_name)
-
-main()
+    if len(sys.argv) != 3:
+        print(f"usage: {sys.argv[0]} infile num_clusters")
+        sys.exit(1)
+    faiss_kmeans(sys.argv[1], int(sys.argv[2]))
 
 
-
-
+if __name__ == "__main__":
+    main()
